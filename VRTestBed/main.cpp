b4beff8c2d389a233fe295b4d9cf201415783f5e
David Clamage <rangsk@gmail.com>
2016-09-28 21:16:24 -0700

    Including and linking to OpenVR via OpenVR.props
    Added precompiled header and main function.

---
+++ VRTestBed/main.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+
+int CALLBACK WinMain(
+	_In_ HINSTANCE hInstance,
+	_In_ HINSTANCE hPrevInstance,
+	_In_ LPSTR     lpCmdLine,
+	_In_ int       nCmdShow
+)
+{
+	return 0;
+}
---
+++ VRTestBed/stdafx.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <cstdint>
+#include <memory>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <algorithm>
+
+#include <Windows.h>
+#include <openvr.h>
